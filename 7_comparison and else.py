2dca97c8d4c345474950acb0975327017ddc74d6
Jasper Oxford <jasper.oxford@gsg.wa.edu.au>
2022-09-08 15:10:31 +0800

    7 comparison and else

---
+++ 7_comparison and else.py
@@ -0,0 +1,21 @@
+
+weight = 190.4
+light_is_on = weight < 195
+
+if light_is_on:
+  print("the light is on!")
+else:
+  print("We're in the dark!")
+
+weight = 190.4
+
+if weight != 195:
+  print("You're under weight :) ")
+
+
+age = 11
+
+if age >= 18:
+  print("You are old as fuck!")
+else:
+  print("You are too young for this ride!")
