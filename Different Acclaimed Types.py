cafecf4f0806c24ec8461c8e117a8e4e4995ccdc
Jasper Oxford <jasper.oxford@gsg.wa.edu.au>
2022-09-12 14:44:28 +0800

    Different Acclaimed Types

---
+++ Different Acclaimed Types.py
@@ -0,0 +1,15 @@
+day = 21
+
+temp = -15
+
+weight = 190.4729433
+
+print(weight - 2)
+
+DifferentYugiohMonsters = 1000
+
+AmountofBraincellsIHave= 2.5 
+
+print(DifferentYugiohMonsters)
+
+print(AmountofBraincellsIHave)
