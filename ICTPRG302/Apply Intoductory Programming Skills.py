606b6a7121236b9cb9e88cbdb6a280e46b152ef8
Jasper Oxford <jasper.oxford@gsg.wa.edu.au>
2022-09-29 09:02:26 +0800

    Added a new code file to Github for the course

---
+++ ICTPRG302/Apply Intoductory Programming Skills.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+
+webbrowser.open('https://www.youtube.com/')
+
+webbrowser.open('https://jaspersyugiohdecks.powerappsportals.com/')
+
+webbrowser.open('https://gsgrammar.zendesk.com/agent/filters/284094107')
+
+webbrowser.open('https://github.com/JasperOxy/ICT40120')
