57996583d2a3bea0be2fc69fbc5147221db4a602
Jasper Oxford <jasper.oxford@gsg.wa.edu.au>
2022-11-01 12:19:47 +0800

    Trying to create a sequence code

---
+++ Sequence_Selection/Selection.py
@@ -0,0 +1,10 @@
+movie = ("Harry Potter and the Goblet of Fire")
+
+TV_Show = ("Rick and Morty")
+
+Favourite_Song = ("Holiday by KSI")
+
+
+print("What is your favourite movie??")
+print("What is your favourite tv show?")
+print("what is your favourite song?")
